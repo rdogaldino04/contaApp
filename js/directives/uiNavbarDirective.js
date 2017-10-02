42d1cee1cac37f337d164d317aa3c4e5d3ce2aec
rgv <rdogaldino04@gmail.com>
2017-10-01 21:14:29 -0300

    refatoração 01102017

---
+++ js/directives/uiNavbarDirective.js
@@ -0,0 +1,13 @@
+angular.module("contas").directive("uiNavbar", function () {
+	return {
+        templateUrl: "view/uiNavbar.html",        
+		restrict: "E",
+		controller: function ($scope, $location) {
+			//$scope.currentNavItem = 'page1';
+			$scope.goto = function(page) {
+				//console.log("Goto " + page);
+				$location.path(page);
+			}
+		}
+	};
+});
